f80f43f5e4169cd9977102e375866d7f45680330
ostrokach <ostrokach@gmail.com>
2017-11-25 17:43:38 -0500

    Add script to wget EBI PMC data in parallel
    This seems to work faster than `lftp`...

---
+++ downloads/ebi/pmc/suppl/parallel_wget_suppl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ev
+
+N_JOBS=64
+
+rm -f .listing index.html*
+wget --no-remove-listing ftp://ftp.ebi.ac.uk/pub/databases/pmc/suppl/NON-OA/
+awk '{print $9}' .listing | tail -n+3 | tr -d '\r' | parallel -j ${N_JOBS} --joblog non-oa-joblob.txt wget --no-verbose -m -nH --cut-dirs=4 ftp://ftp.ebi.ac.uk/pub/databases/pmc/suppl/NON-OA/{}/
+
+rm -f .listing index.html*
+wget --no-remove-listing ftp://ftp.ebi.ac.uk/pub/databases/pmc/suppl/OA/
+awk '{print $9}' .listing | tail -n+3 | tr -d '\r' | parallel -j ${N_JOBS} --joblog oa-joblog.txt wget --no-verbose -m -nH --cut-dirs=4 ftp://ftp.ebi.ac.uk/pub/databases/pmc/suppl/OA/{}/
+
--- downloads/mirror_ebi_pmc_suppl.sh
+++ downloads/mirror_ebi_pmc_suppl.sh
@@ -2,10 +2,14 @@
 
 set -ev
 
+lftp --version
+
+# set net:socket-buffer 33554432
+
 lftp ftp://ftp.ebi.ac.uk <<EOF
-set net:socket-buffer 33554432
 set ftp:sync-mode off
 set cmd:parallel 24
+set mirror:parallel-transfer-count 64
 set mirror:parallel-directories true
-mirror /pub/databases/pmc/suppl/ ./ebi/pmc/suppl/
+mirror -c --only-missing -P24 --log=sync.log /pub/databases/pmc/suppl/ ./ebi/pmc/suppl/
 EOF
